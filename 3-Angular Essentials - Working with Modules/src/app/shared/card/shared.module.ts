639e0ad3932b9ea3f2f7158fc7ed58dbeca7b9b5
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2024-07-10 14:25:29 +0200

    Angular essentials - working with modules

---
+++ 3-Angular Essentials - Working with Modules/src/app/shared/card/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+
+import { CardComponent } from "./card.component";
+
+
+@NgModule({
+  declarations: [CardComponent],
+  exports: [CardComponent]
+})
+export  class SharedModule { }
---
+++ 3-Angular Essentials - Working with Modules/src/app/tasks/tasks.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks.component';
+import { SharedModule } from '../shared/card/shared.module';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [TasksComponent, TaskComponent, NewTaskComponent],
+  exports: [TasksComponent],
+  imports: [CommonModule, FormsModule, SharedModule],
+})
+export class TasksModule {}
