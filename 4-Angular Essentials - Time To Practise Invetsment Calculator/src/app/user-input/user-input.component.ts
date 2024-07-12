874e9abe0fe1315fb16fab74e7c3fbedf8bb69e5
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2024-07-12 13:30:17 +0200

    Investment Calculator App Completion

---
+++ 4-Angular Essentials - Time To Practise Invetsment Calculator/src/app/user-input/user-input.component.ts
@@ -0,0 +1,31 @@
+import { Component, signal } from '@angular/core';
+
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      annualInvestment: +this.enteredAnnualInvestment(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      duration: +this.enteredDuration(),
+    });
+
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+  }
+}
